bec87db972bf88e40dbd0321d5ecf15b7a9686b4
Sam Raker <sam.raker@gmail.com>
2017-07-07 16:06:46 -0400

    tweak settings for heroku

--- roikoi_catalog/roikoi_catalog/settings.py
+++ roikoi_catalog/roikoi_catalog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,15 +82,16 @@ WSGI_APPLICATION = 'roikoi_catalog.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'dev': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'roikoi',
         'USER': 'roikoi',
         'PASSWORD': 'roikoi',
         'HOST': 'localhost',
         'PORT': '5432',
-    }
+    },
 }
+DATABASES['default'].update(dj_databse_url.config(conn_max_age=500))
 
 
 # Password validation
